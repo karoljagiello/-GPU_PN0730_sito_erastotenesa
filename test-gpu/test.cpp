0419da1c4faa00822d80471dd913a9752472f329
Karol Jagiello <karoljagiello.mobile@gmail.com>
2022-11-08 22:49:40 +0100

    prosty hello World

--- test-gpu/test.cpp
+++ test-gpu/test.cpp
@@ -1,27 +1,43 @@
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 
 #include <CL/cl.hpp>
-//#include <iostream>
-
+#include <iostream>
+#include <fstream>
 int main()
 {
 	std::vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
 
-	_ASSERT(platforms.size() > 0);
-
 	auto platform = platforms.front();
 	std::vector<cl::Device> devices;
-	platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+	platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
 
-	_ASSERT(devices.size() > 0);
+
+	auto device = devices.front(); //wybranie pierwszego urz¹dzenia z listy
+
+
+	std::ifstream helloWorldFile("test.cl"); //zaladowanie strumienia pliku tekstowego
+	std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>())); //zaladowanie zawartoœci do stringa
+
+	cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1)); //?
 	
-	int cpt = devices.size();
-	auto device = devices.front();
-	auto vendor = device.getInfo<CL_DEVICE_VENDOR>();
-	auto version = device.getInfo<CL_DEVICE_VERSION>();
+	cl::Context context(device); //urz¹dzenie mog¹ce przetwarzaæ openCL
 
+	cl::Program program(context, sources); //stworzenie programu ze Ÿród³a i contextu
 
+	auto err = program.build("-cl-std=CL1.2"); //budowanie, ewentualny error
 
-	//std::cout << vendor.c_str() << std::endl;
+	char buf[16];
+
+	cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf)); //stworzenie bufora pamiêci z flagami
+
+	cl::Kernel kernel(program, "test", &err); //stworzenie kernela, w stringu nazwa funkcji z pliku .cl
+	kernel.setArg(0, memBuf); //za³adowanie jako argumentu bufora pamiêci
+
+	cl::CommandQueue queue(context, device); //stworzenie komendy
+	queue.enqueueTask(kernel); //za³adowanie komendy
+	queue.enqueueReadBuffer(memBuf, GL_TRUE, 0, sizeof(buf), buf); //odczytanie bufora danych
+
+	std::cout << buf;
+	std::cin.get();
 }
